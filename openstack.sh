dc63fa8bc7858e38a60c799190bb640dc5d39834
Darryl Noye <dcnoye@gmail.com>
2015-07-30 15:50:13 -0400

    drop /dashboard from url

--- openstack.sh
+++ openstack.sh
@@ -466,6 +466,10 @@ yum -y install openstack-dashboard httpd mod_wsgi memcached python-memcached
 sed -i.bak "s/ALLOWED_HOSTS = \['horizon.example.com', 'localhost'\]/ALLOWED_HOSTS = ['*']/" /etc/openstack-dashboard/local_settings
 sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$CONTROLLER_IP"'"/' /etc/openstack-dashboard/local_settings
 
+#drop the /dashboard from the url
+sed -i.bak '%s/WSGIScriptAlias \/dashboard/WSGIScriptAlias \//' /etc/httpd/conf.d/openstack-dashboard.conf  
+
+
 #start dashboard
 setsebool -P httpd_can_network_connect on
 chown -R apache:apache /usr/share/openstack-dashboard/static
